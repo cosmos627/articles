d9f4d6c3da19430f0b8fb53a41dabce108962ee8
leeduhyeon <leeduhyeon12@gmail.com>
2024-10-15 14:39:28 +0900

    작업 패키지 생성 및 DTO만 생성함

--- src/main/java/com/my/articles/controller/ArticleController.java
+++ src/main/java/com/my/articles/controller/ArticleController.java
@@ -1,15 +1,27 @@
 package com.my.articles.controller;
 
+import com.my.articles.entity.Article;
+import com.my.articles.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("articles")
 public class ArticleController {
+    @Autowired
+    ArticleService articleService;
+
     @GetMapping("")
     public String showAllArticles() {
+
         return "/articles/show_all";
     }
 
@@ -19,12 +31,18 @@ public class ArticleController {
     }
 
     @PostMapping("create")
-    public String createArticle() {
-        return "redirect:articles";
+    public ModelAndView createArticle(@RequestParam("title")String title,
+                                      @RequestParam("content")String content,
+                                      Model model) {
+
+        Article article = articleService.createArticle(title, content);
+        model.addAttribute("article", article);
+
+        return new ModelAndView("redirect:articles");
     }
 
     @GetMapping("{id}")
-    public String showOneArticle() {
+    public String showOneArticle(Model model) {
         return "/articles/show";
     }
 
