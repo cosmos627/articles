e069344f02328fe64127c88a9eb89a2ab019e927
cosmos627 <leeduhyeon12@gmail.com>
2024-10-15 15:09:28 +0900

    게시글 전체 보이기 및 개별 보이기 처리 완료 (#6)

--- src/main/java/com/my/articles/dao/ArticleDAO.java
+++ src/main/java/com/my/articles/dao/ArticleDAO.java
@@ -1,7 +1,26 @@
 package com.my.articles.dao;
 
+import com.my.articles.dto.ArticleDTO;
 import com.my.articles.entity.Article;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public interface ArticleDAO {
-    Article insertArticle(String title, String content);
+import java.util.List;
+
+@Component
+public class ArticleDAO {
+    @Autowired
+    EntityManager em;
+
+    public List<Article> getAllArticle() {
+        String sql = "SELECT a FROM Article a " +
+                "ORDER BY a.id DESC";
+
+        return em.createQuery(sql).getResultList();
+    }
+
+    public Article getOneArticle(Long id) {
+        return em.find(Article.class, id);
+    }
 }
